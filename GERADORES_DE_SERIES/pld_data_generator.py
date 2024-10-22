4450d8513d3e4f905ec8ef584035ca7faf30f2d4
Jonathas <jaguiar@id.uff.br>
2024-10-22 14:34:16 -0300

    update

--- GERADORES_DE_SERIES/pld_data_generator.py
+++ GERADORES_DE_SERIES/pld_data_generator.py
@@ -1,41 +1,57 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from pathlib import Path
+
 """ 
     SCRIPT PARA GERAÇÃO DE SÉRIES DE PLD(PREÇO DE LIQUIDAÇÃO DE DIFERENÇA) A PARTIR DE UM HISTÓRICO DE  DADOS.
     FONTE DOS DADOS: <>
 """
 
 # Caminho das séries históricas
-path = 'C:\\Users\\jonat\\OneDrive\\Área de Trabalho\\PROJETO_VPP_II\\GERADORES_DE_SERIES\\BASE_DE_DADOS\\Historico_do_Preco_Horario(SE)_-_17_de_abril_de_2018_a_5_de_abril_de_2022.xlsx'
+path = Path(__file__).parent / 'BASE_DE_DADOS' / 'Historico_do_Preco_Horario(SE)_-_17_de_abril_de_2018_a_5_de_abril_de_2022.xlsx'
 
 # Importando a tabela PLD e convertendo em séries históricas
-PLD_Table = pd.read_excel(path, header = None, index_col = False)
+PLD_Table = pd.read_excel(path, header = None)
 
-# Obtendo os dados das 24 linhas(horas) e das 1437 colunas e atribuindo na váriavel Daely_PLD
+print(PLD_Table.head())
+
+# Obtendo os dados das 24 linhas(horas) e das 1437 colunas e atribuindo na váriavel Daily_PLD
 Daily_PLD = PLD_Table.iloc[1: 24, 2:] # Período de 24 horas
 # Obtendo as dimensões da matriz M = 24 linhas e N = 1437 colunas
-M, N = Daily_PLD.shape
-# Transformando os dados obtidos acima em vetor coluna(série temporal)
-PLD_daily_tsdata = Daily_PLD.values.reshape((M * N, 1)) 
-# Obtendo o tamanho da série temporal por ano
-len_pld_timeseries = len(PLD_daily_tsdata) # 345122
-# Variável com a quantidades de horas em um ano 24h x 365 = 8760h
-Npoints = 8760
-# Obtendo a quantidade de cenários(anos) em um número inteiro
-Nscenarios = len_pld_timeseries // Npoints
-# Criando uma matriz preenchida com zeros com dimenões Ncenários x Npoints
-PLD_hourly_series = np.zeros((Nscenarios, Npoints))
 
-for s in range(Nscenarios):
-    inicio = Npoints * s
-    fim = Npoints * (s + 1)
-    PLD_hourly_series [s, :] = PLD_daily_tsdata[inicio: fim].flatten()
+print(Daily_PLD.head())
 
-# Salvar o DataFrame em um arquivo CSV, Excel ou outro formato
-PLD_hourly_df = pd.DataFrame(PLD_hourly_series)
-PLD_hourly_df.to_csv("C:\\Users\\jonat\\OneDrive\\Área de Trabalho\\PROJETO_VPP_II\\SERIES_GERADAS\\PLD_hourly_series.csv",
-    sep = ';',
-    index = False,
-    header = None
-    )
+# M, N = Daily_PLD.shape
+# # Transformando os dados obtidos acima em vetor coluna(série temporal)
+# PLD_daily_tsdata = Daily_PLD.values.reshape((M * N, 1)) 
+# # Obtendo o tamanho da série temporal por ano
+# len_pld_timeseries = len(PLD_daily_tsdata) # 345122
+# # Variável com a quantidades de horas em um ano 24h x 365 = 8760h
+# Npoints = 8760
+# # Obtendo a quantidade de cenários(anos) em um número inteiro
+# Nscenarios = len_pld_timeseries // Npoints
+# # Criando uma matriz preenchida com zeros com dimenões Ncenários x Npoints
+# PLD_hourly_series = np.zeros((Nscenarios, Npoints))
+
+# for s in range(Nscenarios):
+#     inicio = Npoints * s
+#     fim = Npoints * (s + 1)
+#     PLD_hourly_series [s, :] = PLD_daily_tsdata[inicio: fim].flatten()
+
+# # # # Salvar o DataFrame em um arquivo CSV, Excel ou outro formato
+# # # PLD_hourly_df = pd.DataFrame(PLD_hourly_series)
+# # # PLD_hourly_df.to_csv("C:\\Users\\jonat\\OneDrive\\Área de Trabalho\\PROJETO_VPP_II\\SERIES_GERADAS\\PLD_hourly_series.csv",
+# # #     sep = ';',
+# # #     index = False,
+# # #     header = None
+# # #     )
+
+# print(PLD_hourly_series.shape)
+# if __name__ == '__main__':
+#     import matplotlib.pyplot as plt
+
+#     for i in range(PLD_hourly_series.shape[0]):
+
+#         plt.plot(PLD_hourly_series[i, : 24])
+#         plt.show()
+
--- VPP_DISPATCH_V0/carrega_projecoes_aux.py
+++ VPP_DISPATCH_V0/carrega_projecoes_aux.py
@@ -1,84 +1,94 @@
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
+# from pathlib import Path
+
+# def projecoes(Nt: int, Nl: int, Ndl: int, Npv: int, Nwt: int)-> tuple[np.ndarray, ...]:
+
+#     path = Path(__file__).parent.parent
+#     # c:\Users\jonat\OneDrive\Área de Trabalho\PROJETO_VPP_II
+
+#     inicio = 0
+
+#     # Cargas despacháveis
+#     # path_1 = "C:\\Users\\Jonathas Aguiar\\Desktop\\IC_VPP_II\\SERIES_GERADAS\\dload_hourly_series.xlsx"
+#     path_1 = path / 'SERIES_GERADAS' / 'dload_hourly_series.xlsx'
+#     files = pd.ExcelFile(path_1)
+#     p_l = np.zeros((Ndl, Nt))
+
+#     for i, sheet in enumerate(files.sheet_names):
+
+#         dload_hourly_series = pd.read_excel(path_1, header = None, sheet_name = sheet)
+
+#         m, _ = dload_hourly_series.shape
+#         idx = np.random.choice(m, 1)
+
+#         p_l[i, :] = dload_hourly_series.iloc[idx, inicio: (inicio + Nt)].values / 1e6
+
+#     # Carregamento das séries de potência das usinas solares
+#     # path_2 = 'C:\\Users\\jonat\\OneDrive\\Área de Trabalho\\PROJETO_VPP_II\\SERIES_GERADAS\\PVsystem_hourly_series.xlsx'
+#     path_2 = path / 'SERIES_GERADAS' / 'PVsystem_hourly_series.xlsx'
+
+#     files = pd.ExcelFile(path_2)
+#     p_pv = np.zeros((Npv, Nt))
+
+#     for i, sheet in enumerate(files.sheet_names):
+
+#         PVpwr_hourly_series = pd.read_excel(path_2, header = None, sheet_name = sheet)
+#         m, _ = PVpwr_hourly_series.shape
+#         idx = np.random.choice(m, 1)
+
+#         p_pv[i, :] = PVpwr_hourly_series.iloc[idx,  inicio: (inicio + Nt)].values / 1e6
+
+
+#     return p_l, p_pv
+
+# # Exemplos de uso
+# if __name__ == '__main__':
+
+#     import matplotlib.pyplot as plt
+#     from vpp_data import vpp
+
+#     data = vpp()
+
+#     Nt = 24  # Número de pontos de dados na série temporal
+#     Nl = data['Nl']   # Número de cargas
+#     Ndl = data['Ndl'] # Número de cargas de referência
+#     Npv = data['Npv']  # Número de sistemas fotovoltaicos
+#     Nwt = data['Nwt']  # Número de sistemas de geração eólica
+
+   
+
+#     p_l, p_pv = projecoes(Nt, Nl, Ndl, Npv, Nwt)
+
+#     # plot das séries de potência das cargas despacháveis
+#     for i in range(Ndl):
+
+#         title = f'Carga Despachável {i + 1}'
+#         plt.figure(figsize = (10, 4))
+#         plt.title(title)
+#         plt.plot(p_l[i, :], 'r')
+#         plt.xlabel('hora')
+#         plt.ylabel('potência em MW')
+#         plt.show()
+   
+#     # plot das séries de potência das usinas solares
+#     for i in range(Npv):
+
+#         title = f'Usina Solar{i + 1}'
+#         plt.figure(figsize = (10, 4))
+#         plt.title(title)
+#         plt.plot(p_pv[i, :], 'r')
+#         plt.xlabel('hora')
+#         plt.ylabel('potência em MW')
+#         plt.show()
+   
 from pathlib import Path
+import pandas as pd
 
-def projecoes(Nt: int, Nl: int, Ndl: int, Npv: int, Nwt: int)-> tuple[np.ndarray, ...]:
+path = Path(__file__).parent.parent / 'SERIES_GERADAS' / 'PVsystem_hourly_series.xlsx'
 
-    path = Path(__file__).parent.parent
-    # c:\Users\jonat\OneDrive\Área de Trabalho\PROJETO_VPP_II
+sheet = pd.ExcelFile(path)
 
-    inicio = 0
+print(path)
 
-    # Cargas despacháveis
-    # path_1 = "C:\\Users\\Jonathas Aguiar\\Desktop\\IC_VPP_II\\SERIES_GERADAS\\dload_hourly_series.xlsx"
-    path_1 = path / 'SERIES_GERADAS' / 'dload_hourly_series.xlsx'
-    files = pd.ExcelFile(path_1)
-    p_l = np.zeros((Ndl, Nt))
-
-    for i, sheet in enumerate(files.sheet_names):
-
-        dload_hourly_series = pd.read_excel(path_1, header = None, sheet_name = sheet)
-
-        m, _ = dload_hourly_series.shape
-        idx = np.random.choice(m, 1)
-
-        p_l[i, :] = dload_hourly_series.iloc[idx, inicio: (inicio + Nt)].values / 1e6
-
-    # Carregamento das séries de potência das usinas solares
-    # path_2 = 'C:\\Users\\jonat\\OneDrive\\Área de Trabalho\\PROJETO_VPP_II\\SERIES_GERADAS\\PVsystem_hourly_series.xlsx'
-    path_2 = path / 'SERIES_GERADAS' / 'PVsystem_hourly_series.xlsx'
-
-    files = pd.ExcelFile(path_2)
-    p_pv = np.zeros((Npv, Nt))
-
-    for i, sheet in enumerate(files.sheet_names):
-
-        PVpwr_hourly_series = pd.read_excel(path_2, header = None, sheet_name = sheet)
-        m, _ = PVpwr_hourly_series.shape
-        idx = np.random.choice(m, 1)
-
-        p_pv[i, :] = PVpwr_hourly_series.iloc[idx,  inicio: (inicio + Nt)].values / 1e6
-
-
-    return p_l, p_pv
-
-# Exemplos de uso
-if __name__ == '__main__':
-
-    import matplotlib.pyplot as plt
-    from vpp_data import vpp
-
-    data = vpp()
-
-    Nt = 24  # Número de pontos de dados na série temporal
-    Nl = data['Nl']   # Número de cargas
-    Ndl = data['Ndl'] # Número de cargas de referência
-    Npv = data['Npv']  # Número de sistemas fotovoltaicos
-    Nwt = data['Nwt']  # Número de sistemas de geração eólica
-
-   
-
-    p_l, p_pv = projecoes(Nt, Nl, Ndl, Npv, Nwt)
-
-    # plot das séries de potência das cargas despacháveis
-    for i in range(Ndl):
-
-        title = f'Carga Despachável {i + 1}'
-        plt.figure(figsize = (10, 4))
-        plt.title(title)
-        plt.plot(p_l[i, :], 'r')
-        plt.xlabel('hora')
-        plt.ylabel('potência em MW')
-        plt.show()
-   
-    # plot das séries de potência das usinas solares
-    for i in range(Npv):
-
-        title = f'Usina Solar{i + 1}'
-        plt.figure(figsize = (10, 4))
-        plt.title(title)
-        plt.plot(p_pv[i, :], 'r')
-        plt.xlabel('hora')
-        plt.ylabel('potência em MW')
-        plt.show()
-   
+print(sheet)
