3ade8644f428ede3b9309d777c027a50933f2eed
jaguiar <jaguiar@id.uff.br>
2024-10-22 09:50:04 -0300

    update in carrega_projecoes

--- VPP_DISPATCH_V0/carrega_projecoes_aux.py
+++ VPP_DISPATCH_V0/carrega_projecoes_aux.py
@@ -1,14 +1,19 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
-def carrega(Nt: int|None, Nl: int|None, Ndl: int|None, Npv: int|None, Nwt: int|None)-> tuple[np.ndarray, ...]:
+def projecoes(Nt: int, Nl: int, Ndl: int, Npv: int, Nwt: int)-> tuple[np.ndarray, ...]:
+
+    path = Path(__file__).parent.parent
+    # c:\Users\jonat\OneDrive\Área de Trabalho\PROJETO_VPP_II
 
     inicio = 0
 
-    path_1 = "C:\\Users\\Jonathas Aguiar\\Desktop\\IC_VPP_II\\SERIES_GERADAS\\dload_hourly_series.xlsx"
-    
+    # Cargas despacháveis
+    # path_1 = "C:\\Users\\Jonathas Aguiar\\Desktop\\IC_VPP_II\\SERIES_GERADAS\\dload_hourly_series.xlsx"
+    path_1 = path / 'SERIES_GERADAS' / 'dload_hourly_series.xlsx'
     files = pd.ExcelFile(path_1)
-    p_l = np.zeros(Ndl, Nt)
+    p_l = np.zeros((Ndl, Nt))
 
     for i, sheet in enumerate(files.sheet_names):
 
@@ -17,14 +22,63 @@ def carrega(Nt: int|None, Nl: int|None, Ndl: int|None, Npv: int|None, Nwt: int|N
         m, _ = dload_hourly_series.shape
         idx = np.random.choice(m, 1)
 
-        p_l[i, :] = dload_hourly_series.iloc[idx, inicio: (inicio + Nt)].values
+        p_l[i, :] = dload_hourly_series.iloc[idx, inicio: (inicio + Nt)].values / 1e6
+
+    # Carregamento das séries de potência das usinas solares
+    # path_2 = 'C:\\Users\\jonat\\OneDrive\\Área de Trabalho\\PROJETO_VPP_II\\SERIES_GERADAS\\PVsystem_hourly_series.xlsx'
+    path_2 = path / 'SERIES_GERADAS' / 'PVsystem_hourly_series.xlsx'
+
+    files = pd.ExcelFile(path_2)
+    p_pv = np.zeros((Npv, Nt))
+
+    for i, sheet in enumerate(files.sheet_names):
+
+        PVpwr_hourly_series = pd.read_excel(path_2, header = None, sheet_name = sheet)
+        m, _ = PVpwr_hourly_series.shape
+        idx = np.random.choice(m, 1)
+
+        p_pv[i, :] = PVpwr_hourly_series.iloc[idx,  inicio: (inicio + Nt)].values / 1e6
 
 
+    return p_l, p_pv
 
+# Exemplos de uso
+if __name__ == '__main__':
 
-    return p_l
+    import matplotlib.pyplot as plt
+    from vpp_data import vpp
 
-Nt = 24
-Ndl = 3
+    data = vpp()
 
-p_l = carrega(Nt, Nl = None, Ndl, Npv = None)
+    Nt = 24  # Número de pontos de dados na série temporal
+    Nl = data['Nl']   # Número de cargas
+    Ndl = data['Ndl'] # Número de cargas de referência
+    Npv = data['Npv']  # Número de sistemas fotovoltaicos
+    Nwt = data['Nwt']  # Número de sistemas de geração eólica
+
+   
+
+    p_l, p_pv = projecoes(Nt, Nl, Ndl, Npv, Nwt)
+
+    # plot das séries de potência das cargas despacháveis
+    for i in range(Ndl):
+
+        title = f'Carga Despachável {i + 1}'
+        plt.figure(figsize = (10, 4))
+        plt.title(title)
+        plt.plot(p_l[i, :], 'r')
+        plt.xlabel('hora')
+        plt.ylabel('potência em MW')
+        plt.show()
+   
+    # plot das séries de potência das usinas solares
+    for i in range(Npv):
+
+        title = f'Usina Solar{i + 1}'
+        plt.figure(figsize = (10, 4))
+        plt.title(title)
+        plt.plot(p_pv[i, :], 'r')
+        plt.xlabel('hora')
+        plt.ylabel('potência em MW')
+        plt.show()
+   
